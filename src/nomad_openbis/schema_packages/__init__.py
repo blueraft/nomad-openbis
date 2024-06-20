3bbf4dff902863ba94688fd50258327aa9f768ae
Amir Golparvar <golparvar.amir@gmail.com>
2024-06-20 12:13:22 +0200

    tests

--- src/nomad_openbis/schema_packages/__init__.py
+++ src/nomad_openbis/schema_packages/__init__.py
@@ -1,9 +1,7 @@
 from nomad.config.models.plugins import SchemaPackageEntryPoint
-from pydantic import Field
 
 
 class OpenbisEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
         from nomad_openbis.schema_packages.openbis import m_package
